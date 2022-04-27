fc68d0f006425c85e930401c2cc7d80f734a4b6a
mjms0214 <mjms0214@sookmyung.ac.kr>
2022-04-27 20:13:23 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+// admin, weing25
+// mongodb+srv://admin:<password>@weing.jl8pj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const mongoose= require('mongoose');
+mongoose.connect('mongodb+srv://admin:weing25@weing.jl8pj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected..'))
+.catch(err => console.log(err))
+
+app.listen(8080, function(){
+    console.log('listening on 8080');
+});
+
+app.get('/', function(req, res){
+    res.send('메인 페이지');
+})
