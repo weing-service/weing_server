7a80bb50a325aa2ea252b1e158ad119715c762c9
JAEHEEE <y2hjjh@naver.com>
2022-05-26 20:43:47 +0900

    Update: 프로젝트 코드 수정

--- router/project.js
+++ router/project.js
@@ -16,11 +16,11 @@ router.post(
 router.post("/coverImg", upload.single("coverImage"), projectCtrl.coverImg) // 커버 이미지 등록
 router.post("/", projectCtrl.createProject) // 프로젝트 생성
 router.get("/", projectCtrl.getAllProjects) // 로그인한 유저의 프로젝트 모두 불러오기
-router.put("/:projectId", projectCtrl.editProject) // 프로젝트 수정
-router.delete("/:projectId", projectCtrl.delProject) // 프로젝트 삭제
-router.get("/:projectId", projectCtrl.getProject) // 프로젝트 하나 불러오기
+router.put("/", projectCtrl.editProject) // 프로젝트 수정
+router.delete("/", projectCtrl.delProject) // 프로젝트 삭제
+router.get("/one", projectCtrl.getProject) // 프로젝트 하나 불러오기
 router.get("/:project/:date", projectCtrl.countSchedule) // 예정된 일정 개수, 완료된 일정 개수 세기
-router.post("/:projectId/user/:userId", projectCtrl.addUser) // 프로젝트 팀원 추가
-router.put("/:projectId/user/:username", projectCtrl.delUser) // 프로젝트 팀원 삭제
+router.post("/:username", projectCtrl.addUser) // 프로젝트 팀원 추가
+router.put("/:username", projectCtrl.delUser) // 프로젝트 팀원 삭제
 
 module.exports = router
